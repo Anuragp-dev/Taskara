f404de1663938f93cd2fd633c44169c9cea6f211
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2024-10-01 16:59:19 +0530

    changes home page

--- client/app/Components/filters/Filters.tsx
+++ client/app/Components/filters/Filters.tsx
@@ -7,12 +7,32 @@ import React from 'react'
 
 const Filters = () => {
 
+    const [activeIndex, setActiveIndex] = React.useState(0);
     const { priority, setPriority } = useTasks();
 
     const priorities = ["All", "Medium", "Low", "High",];
     return (
         <div className='relative py-2 grid grid-cols-4 items-center gap-3 bg-[#F9F9F9] border-2 border-white rounded-md' >
-            filter
+            <span className='absolute left-[5px] bg-[#EDEDED] rounded-md transition-all duration-300 '
+                style={{
+                    width: "calc(100%/4 - 10px)",
+                    height: "calc(100% - 10px)",
+                    top: "50%",
+                    transform: "translate(0,-50%)"
+                }}></span>
+            {priorities.map((priority, index) => (
+                <button
+                    key={index}
+                    className={`relative px-1 z-10 front-medium text-sm ${activeIndex === index ? 'text-[#3aafae]' : 'text-gray-500'} `}
+                    onClick={() => {
+                        setActiveIndex(index);
+                        setPriority(priority.toLocaleLowerCase());
+                    }}
+                >
+                    {priority}
+                </button>
+            ))
+            }
         </div>
     )
 }
