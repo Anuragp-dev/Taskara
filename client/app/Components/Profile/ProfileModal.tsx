eec22fa3bf70fd6e2e4375692103100a9c83cc63
anuragRexav <107052990+Anuragp-dev@users.noreply.github.com>
2024-10-11 15:13:15 +0530

    changes in modal

--- client/app/Components/Profile/ProfileModal.tsx
+++ client/app/Components/Profile/ProfileModal.tsx
@@ -2,7 +2,7 @@
 import { useTasks } from '@/context/taskContext'
 import { useUserContext } from '@/context/userContext'
 import useDetectOutside from '@/hooks/useDetectOutside'
-import { badge, check, github } from '@/Utils/Icons'
+import { badge, check, github, mail } from '@/Utils/Icons'
 import Image from 'next/image'
 import React from 'react'
 
@@ -23,6 +23,18 @@ const ProfileModal = () => {
     // profile section
 
     const { name, email, Photo } = user;
+
+    const [oldPassword, setOldPassword] = React.useState('');
+    const [newPassword, setNewPassword] = React.useState('');
+
+
+    const handlePassword = (type: string) => () => (e: any) => {
+        if (type === 'old') {
+            setOldPassword(e.target.value)
+        } else {
+            setNewPassword(e.target.value)
+        }
+    }
     return (
 
         <div className='fixed left-0 top-0  z-50 h-full w-full bg-[#333]/30 overflow-hidden' >
@@ -58,8 +70,63 @@ const ProfileModal = () => {
                             {check} verified
                         </button>
                     </div>
-
                 </div>
+                <div>
+                    <h1 className='text-lg font-bold '>{name}</h1>
+                    <p className='text-sm text-gray-500'>{email}</p>
+                </div>
+
+                <form
+                    action=""
+                    className='mt-4 pt-2 flex flex-col gap-3 border-t-2 border-t-[#323232]/10'
+                >
+                    <div className='pt-2 grid grid-cols-[150px_1fr]'>
+                        <label htmlFor='name' className='text-sm font-medium'>
+                            Full Name
+                        </label>
+                        <input
+                            type='text'
+                            id='name'
+                            name='name'
+                            defaultValue={name}
+                            onChange={(e) => handlerUserInput("name")(e)}
+                            className='py-[0.4rem] px-3 font-medium rounded-lg border-2 border-[#323232]/10'
+                        />
+
+                    </div>
+                    <div className='pt-2 grid grid-cols-[150px_1fr]'>
+                        <label htmlFor='name' className='text-sm font-medium'>
+                            Email Address
+                        </label>
+                        <div className='relative w-full'>
+                            <input
+                                type='text'
+                                id='email'
+                                name='email'
+                                defaultValue={email}
+                                onChange={(e) => handlerUserInput("name")(e)}
+                                className='w-full py-[0.4rem] pl-9 pr-2 font-medium rounded-lg border-2 border-[#323232]/10'
+                            />
+                            <span className='absolute left-0 top-0 bottom-0 flex items-center px-3 text-[#323232]/50'>
+                                {mail}
+                            </span>
+                        </div>
+                    </div>
+                    <div className='pt-4 grid grid-col-2 gap-4 border-t-2 border-t-[#323232]/10'>
+                        <div className='flex flex-col gap-1'>
+                            <label htmlFor='oldPassword' className='text-sm font-medium'>
+                                Old Password
+                            </label>
+                            <input
+                                type="password"
+                                id='oldPassword'
+                                value={oldPassword}
+                                onChange={handlePassword("old")}
+                                className='py-[0.4rem] px-3 front-medium rounded-lg border-2 border-[#323232]/10'
+                            />
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
 
