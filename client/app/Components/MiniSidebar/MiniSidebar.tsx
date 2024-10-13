a645eb3546e6fc5d4d73345c443653558bd80672
ANURAG P <anuragp212@gmail.com>
2024-10-14 00:01:36 +0530

    Update MiniSidebar.tsx

--- client/app/Components/MiniSidebar/MiniSidebar.tsx
+++ client/app/Components/MiniSidebar/MiniSidebar.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react'
-
+//side bar
 const MiniSidebar = () => {
 
   const pathname = usePathname();
