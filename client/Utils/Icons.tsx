69baf7d9c1ea8b2bd1e728806188353e7a29a1e1
ANURAG P <anuragp212@gmail.com>
2024-10-28 15:46:06 +0530

    Update Icons.tsx

--- client/Utils/Icons.tsx
+++ client/Utils/Icons.tsx
@@ -1,4 +1,4 @@
-// ico
+// icon
 export const github = <i className="fa-brands fa-github"></i>;
 export const star = <i className="fas fa-star"></i>;
 export const trash = <i className="fas fa-trash"></i>;
