989034781d5a32da45fdc7f35162551e547382bf
ANURAG P <anuragp212@gmail.com>
2025-01-14 11:44:03 +0530

    Update adminController.js

--- backend/src/controllers/auth/adminController.js
+++ backend/src/controllers/auth/adminController.js
@@ -5,7 +5,7 @@ import User from "../../models/auth/UserModel.js";
 export const deleteUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
-  // attempt to find and delete the users
+  // attempt to find and delete the users 
   try {
     const user = await User.findByIdAndDelete(id);
     if (!user) {
