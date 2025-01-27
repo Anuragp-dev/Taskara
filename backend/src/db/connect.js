e9e65d7f492f98fa9ea05b33e7a437a2a2afad5d
ANURAG P <anuragp212@gmail.com>
2025-01-27 10:16:58 +0530

    Update connect.js

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     console.log("Attempting to connect to database.....");
+    
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to database.....");
   } catch (error) {
